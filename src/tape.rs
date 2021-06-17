d9a100de9e2bb1f2bca875ef4c71fbb18ae057db
amarrerod <alemarrerodiaz@gmail.com>
2021-06-17 14:27:28 +0100

    ✨ Includes the get&set for Tape

--- src/tape.rs
+++ src/tape.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
@@ -6,13 +5,30 @@ use std::io::BufReader;
 
 #[derive(Debug)]
 pub struct Tape {
-    content: Vec<char>,
-    pos: u32,
+    content:  Vec<char>,
+    pos:  u32,
 }
 
 impl Tape {
     pub fn new(content: Vec<char>, pos: u32) -> Tape {
         Tape { content, pos }
+    }
+
+    pub fn get_pos(&self) -> u32 { self.pos }
+
+    pub fn update_pos(&mut self, new_pos: u32) {
+        if new_pos >= self.content.len() as u32 {
+            panic!("Error while updating the position in the tape with the value: {}", new_pos);
+        }
+        self.pos = new_pos
+    }
+
+    pub fn set_char_at_pos(&mut self, new_char: char) {
+        self.content[self.pos as usize] = new_char
+    }
+
+    pub fn get_char_at_pos(&self) -> char {
+        self.content[self.pos as usize]
     }
 }
 
