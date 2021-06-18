3070b19f3d32573605846c642b41f60245e5746d
amarrerod <alemarrerodiaz@gmail.com>
2021-06-18 15:48:53 +0100

    ✅  Include State tests

--- src/test.rs
+++ src/test.rs
@@ -1,15 +1,22 @@
-
-use crate::tuple::Tuple as Tuple;
-use crate::state::State as State;
+use crate::state::State;
 use crate::tuple::Moves;
+use crate::tuple::Tuple;
 #[cfg(test)]
-
 #[test]
-fn tuple_creating() {
+fn tuple_creation() {
     let tuple: Tuple = Tuple::new(State::new(0), 'A', 'B', Moves::S, State::new(1));
     assert_eq!(tuple.read_symb, 'A');
     assert_eq!(tuple.write_symb, 'B');
     assert_eq!(tuple.state, State::new(0));
     assert_eq!(tuple._move, Moves::S);
     assert_eq!(tuple.next_state, State::new(1));
+}
+
+#[test]
+fn state_creation() {
+    for i in 0..100 {
+        let state: State = State::new(i);
+        assert_eq!(state.id(), i);
+        assert_ne!(state, State::new(i + 1));
+    }
 }
